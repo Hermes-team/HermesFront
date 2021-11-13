267080cc039d5c3a1e5b821331e2707f73ba3006
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-11-13 01:42:06 +0100

    messages list section in Chat Screen

--- lib/views/chat_detail_page.dart
+++ lib/views/chat_detail_page.dart
@@ -1,60 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:front/models/chat_message_model.dart';
 
-class ChatDetailPage extends StatefulWidget{
+class ChatDetailPage extends StatefulWidget {
   @override
   _ChatDetailPageState createState() => _ChatDetailPageState();
 }
 
 class _ChatDetailPageState extends State<ChatDetailPage> {
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+        messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xff20464f),
-        appBar: AppBar(
-          elevation: 0,
-          automaticallyImplyLeading: false,
-          backgroundColor: const Color(0xff213339),
-          flexibleSpace: SafeArea(
-            child: Container(
-              padding: const EdgeInsets.only(right: 16),
-              child: Row(
-                children: <Widget>[
-                  IconButton(
-                    onPressed: (){
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.arrow_back,color: Color(0xffaacaa8),),
+      backgroundColor: const Color(0xFF223239),
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0xff213339),
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.only(right: 16),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Color(0xff8D8D8D),
                   ),
-                  const SizedBox(width: 2,),
-                  // CircleAvatar(
-                  //   backgroundImage: NetworkImage("<https://randomuser.me/api/portraits/men/5.jpg>"),
-                  //   maxRadius: 20,
-                  // ),
-                  const SizedBox(width: 12,),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Text("Kriss Benwat",style: TextStyle(color: Color(0xffaacaa8), fontSize: 16 ,fontWeight: FontWeight.w600),),
-                        const SizedBox(height: 6,),
-                        Text("Online",style: TextStyle(color: Colors.grey.shade600, fontSize: 13),),
-                      ],
-                    ),
+                ),
+                const SizedBox(
+                  width: 2,
+                ),
+                const CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "<https://randomuser.me/api/portraits/men/5.jpg>"),
+                  maxRadius: 20,
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        "Kriss Benwat",
+                        style: TextStyle(
+                            color: Color(0xffEBEBEB),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        "Online",
+                        style: TextStyle(
+                            color: Colors.grey.shade600, fontSize: 13),
+                      ),
+                    ],
                   ),
-                  // Icon(Icons.settings,color: Colors.black54,),
-                ],
-              ),
+                ),
+                const Icon(
+                  Icons.settings,
+                  color: Color(0xff8D8D8D),
+                ),
+              ],
             ),
           ),
         ),
+      ),
       body: Stack(
         children: <Widget>[
+          ListView.builder(
+            itemCount: messages.length,
+            shrinkWrap: true,
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                padding:
+                    EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+                child: Align(
+                  alignment: (messages[index].messageType == "receiver"
+                      ? Alignment.topLeft
+                      : Alignment.topRight),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (messages[index].messageType == "receiver"
+                          ? Color(0xff2A454e)
+                          : Color(0xFF294a2d)),
+                    ),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      messages[index].messageContent,
+                      style: TextStyle(fontSize: 15, color: Color(0xffc9c9c9)),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
           Align(
             alignment: Alignment.bottomLeft,
             child: Container(
-              padding: const EdgeInsets.only(left: 10,bottom: 10,top: 10),
+              padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
               height: 60,
               width: double.infinity,
               color: const Color(0xff2A454e),
@@ -73,26 +137,31 @@ class _ChatDetailPageState extends State<ChatDetailPage> {
                   //     child: Icon(Icons.add, color: Colors.white, size: 20, ),
                   //   ),
                   // ),
-                  const SizedBox(width: 15,),
+                  const SizedBox(
+                    width: 15,
+                  ),
                   const Expanded(
                     child: TextField(
                       decoration: InputDecoration(
-
                           hintText: "Write message...",
-                          hintStyle: TextStyle(color: Color(0xffaacaa8)),
-                          border: InputBorder.none
-                      ),
+                          hintStyle: TextStyle(color: Color(0xFFc9c9c9)),
+                          border: InputBorder.none),
                     ),
                   ),
-                  const SizedBox(width: 15,),
+                  const SizedBox(
+                    width: 15,
+                  ),
                   FloatingActionButton(
-                    onPressed: (){},
-                    child: const Icon(Icons.send,color: Color(0xff213339),size: 18,),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.send,
+                      color: Color(0xff213339),
+                      size: 18,
+                    ),
                     backgroundColor: const Color(0xffaacaa8),
                     elevation: 0,
                   ),
                 ],
-
               ),
             ),
           ),
