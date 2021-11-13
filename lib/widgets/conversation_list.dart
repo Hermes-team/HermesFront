b2eff8914a43466127efc97fcb4eb1d12433a6fb
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-11-13 01:17:03 +0100

    format code

--- lib/widgets/conversation_list.dart
+++ lib/widgets/conversation_list.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:front/views/chat_detail_page.dart';
 
-class ConversationList extends StatefulWidget{
+class ConversationList extends StatefulWidget {
   String name;
   String messageText;
   String imageUrl;
   String time;
   bool isMessageRead;
-  ConversationList({required this.name,required this.messageText,required this.imageUrl,required this.time,required this.isMessageRead});
+
+  ConversationList(
+      {required this.name,
+      required this.messageText,
+      required this.imageUrl,
+      required this.time,
+      required this.isMessageRead});
+
   @override
   _ConversationListState createState() => _ConversationListState();
 }
@@ -16,42 +23,66 @@ class _ConversationListState extends State<ConversationList> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context){
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
           return ChatDetailPage();
         }));
       },
       child: Container(
-          padding: EdgeInsets.only(left: 16,right: 16,top: 10,bottom: 10),
-          child: Row(
+        padding: const EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
+        child: Row(
           children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(widget.imageUrl),
+                    maxRadius: 30,
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
                   Expanded(
-                      child: Row(
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          CircleAvatar(
-                                backgroundImage: NetworkImage(widget.imageUrl),
-                            maxRadius: 30,
+                          Text(
+                            widget.name,
+                            style: const TextStyle(
+                                fontSize: 16, color: Color(0xffEBEBEB)),
                           ),
-                          SizedBox(width: 16,),
-                          Expanded(
-                              child: Container(
-                                  color: Colors.transparent,
-                                  child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                          Text(widget.name, style: TextStyle(fontSize: 16, color: Color(0xffa1cca5)),),
-                                          SizedBox(height: 6,),
-                                          Text(widget.messageText,style: TextStyle(fontSize: 13,color: Color(0xff8d8d8d), fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal),),
-                                          ],
-                                      ),
-                                  ),
-                              ),
-                          ],
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            widget.messageText,
+                            style: TextStyle(
+                                fontSize: 13,
+                                color: const Color(0xff8d8d8d),
+                                fontWeight: widget.isMessageRead
+                                    ? FontWeight.bold
+                                    : FontWeight.normal),
+                          ),
+                        ],
                       ),
-                ),
-            Text(widget.time,style: TextStyle(fontSize: 12,fontWeight: widget.isMessageRead?FontWeight.bold:FontWeight.normal, color: Color(0xff8d8d8d)),),
-            ],
-          ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              widget.time,
+              style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: widget.isMessageRead
+                      ? FontWeight.bold
+                      : FontWeight.normal,
+                  color: const Color(0xff8d8d8d)),
+            ),
+          ],
+        ),
       ),
     );
   }
