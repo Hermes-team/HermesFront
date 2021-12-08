1640d9e6101edc33416c45ddf3c8a4e30181e239
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-12-08 03:21:17 +0100

    fixed search bar on home screen

--- lib/views/chat_page.dart
+++ lib/views/chat_page.dart
@@ -87,26 +87,19 @@ class _ChatPageState extends State<ChatPage> {
         backgroundColor: const Color(0xff213339),
         title: Center(
           child: Container(
-            padding: const EdgeInsets.only(right: 5),
             child: Row(
               children: <Widget>[
-                Expanded(
+                const Expanded(
                   child: TextField(
                     decoration: InputDecoration(
+                      border: InputBorder.none,
                       hintText: "Search...",
-                      hintStyle: const TextStyle(color: Color(0xff8D8D8D)),
-                      prefixIcon: const Icon(
+                      hintStyle: TextStyle(color: Color(0xff8D8D8D)),
+                      prefixIcon: Icon(
                         Icons.search,
-                        color: Color(0xff000000),
-                        size: 20,
+                        color: Color(0xff8D8D8D),
+                        size: 28,
                       ),
-                      filled: true,
-                      fillColor: Colors.white12,
-                      contentPadding: const EdgeInsets.all(2),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide:
-                              const BorderSide(color: Color(0xff1b343A))),
                     ),
                   ),
                 ),
@@ -117,10 +110,9 @@ class _ChatPageState extends State<ChatPage> {
                   icon: const Icon(
                     Icons.settings,
                     color: Color(0xff8D8D8D),
+                    size: 30,
                   ),
                 ),
-                // const Spacer(
-                // ),
               ],
             ),
           ),
@@ -131,28 +123,6 @@ class _ChatPageState extends State<ChatPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            // SafeArea(
-            //   child: Padding(
-            //     padding: const EdgeInsets.only(top: 15, left: 16, right: 70),
-            //     child: TextField(
-            //       decoration: InputDecoration(
-            //         hintText: "Search...",
-            //         hintStyle: const TextStyle(color: Color(0xff8D8D8D)),
-            //         prefixIcon: const Icon(
-            //           Icons.search,
-            //           color: Color(0xff000000),
-            //           size: 20,
-            //         ),
-            //         filled: true,
-            //         //fillColor: const Color(0xff1b343A),
-            //         contentPadding: const EdgeInsets.all(8),
-            //         enabledBorder: OutlineInputBorder(
-            //             borderRadius: BorderRadius.circular(20),
-            //             borderSide: const BorderSide(color: Color(0xff1b343A))),
-            //       ),
-            //     ),
-            //   ),
-            // ),
             SafeArea(
               child: Padding(
                 padding: const EdgeInsets.only(top: 0, left: 10, right: 10),
