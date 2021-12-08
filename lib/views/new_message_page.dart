60dbe42c3fb071bb8b1250d6ccd5e6a7906e8edc
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-12-08 04:40:58 +0100

    added New Message screen

---
+++ lib/views/new_message_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:front/views/private_chat_page.dart';
+
+class NewMessagePage extends StatefulWidget {
+  @override
+  _NewMessagePageState createState() => _NewMessagePageState();
+}
+
+class _NewMessagePageState extends State<NewMessagePage> {
+  List<Map<String, dynamic>> contacts = [
+    {"name": "Liam", "img": "assets/imgs/p2.png"},
+    {"name": "Olivia", "img": "assets/imgs/p3.png"},
+    {"name": "Ava", "img": "assets/imgs/p6.png"},
+    {"name": "Oscar", "img": "assets/imgs/p4.png"},
+    {"name": "Ivy", "img": "assets/imgs/p1.png"},
+    {"name": "Jack", "img": "assets/imgs/p5.png"},
+    {"name": "Liam", "img": "assets/imgs/p2.png"},
+    {"name": "Olivia", "img": "assets/imgs/p3.png"},
+    {"name": "Ava", "img": "assets/imgs/p6.png"},
+    {"name": "Oscar", "img": "assets/imgs/p4.png"},
+    {"name": "Ivy", "img": "assets/imgs/p1.png"},
+    {"name": "Jack", "img": "assets/imgs/p5.png"},
+    {"name": "Liam", "img": "assets/imgs/p2.png"}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: const Color(0xFF223239),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color(0xFF182226),
+        // toolbarHeight: 65,
+        iconTheme: const IconThemeData(
+          color: Colors.grey,
+        ),
+        title: const Text(
+          'New Message',
+          style: TextStyle(fontSize: 22, color: Color(0xffEBEBEB), fontWeight: FontWeight.w900),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Color(0xff8D8D8D)),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Color(0xff8D8D8D),
+                    size: 28,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: contacts.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 3),
+                        child: ListTile(
+                          // onTap: () => navigatePage(
+                          //   context,
+                          //   PrivateChatPage(
+                          //       name: contacts[index]["name"],
+                          //       img: contacts[index]["img"]),
+                          // ),
+                          onTap: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) {
+                              return PrivateChatPage();
+                            }));
+                          },
+                          title: Text(
+                            "${contacts[index]["name"]}",
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          ),
+                          contentPadding: EdgeInsets.all(4),
+                          leading: CircleAvatar(
+                            maxRadius: 32.0,
+                            child: Image.asset(
+                              "${contacts[index]["img"]}",
+                            ),
+                          ),
+                        ),
+                      );
+                    })),
+          ],
+        ),
+      ),
+    );
+  }
+}
